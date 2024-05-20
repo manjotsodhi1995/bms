f73203249f98b99f9da524cc111b0859299bc8a0
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2024-05-20 13:47:54 +0530

    ok

--- src/components/affiliate/Hero.tsx
+++ src/components/affiliate/Hero.tsx
@@ -4,7 +4,7 @@ import ExpandableBox from "../Expandable";
 function Hero() {
   return (
     <div className="lg:px-[15%] xl:px-[15%] px-[10vw] md:py-[7vh] pb-16 flex flex-col gap-16">
-      <div className="flex justify-around gap-4 md:flex-row flex-col">
+      <div className="flex justify-around gap-8 md:flex-row flex-col">
         <div className="md:w-[40%] items-center md:flex hidden">
           <img src={HeroImg} alt="" />
         </div>
@@ -37,7 +37,7 @@ function Hero() {
         </div>
       </div>
       <div className="flex flex-col justify-around gap-16 items-center py-16 mt-8">
-        <div className="font-semibold lg:text-[2rem] 2xl:text-[2.5rem] md:w-[50%] text-center text-[1.8rem] leading-tight">
+        <div className="font-semibold lg:text-[2rem] 2xl:text-[2.5rem] md:w-[60%] text-center text-[1.8rem] leading-tight">
           Why promote EventBookings to your audience?
         </div>
         <div className="flex justify-around gap-4 md:flex-row flex-col">
