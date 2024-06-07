e728ca61179cb3d377fc62be5d2dac29979ca51a
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2024-06-07 19:12:51 +0530

    ok

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <div className="max-w-screen overflow-hidden">
         <div className="overflow-x-hidden max-w-screen">
           <div className="overflow-x-hidden absolute rounded-full w-[70vw] h-[70vw] bg-gradient-to-br from-[#A87863] via-[#C68D55] to-[#C5C168] bg-opacity-20 backdrop-blur-[700px] animate-float -z-10 top-[-50vw] left-[-25vw]"></div>
-          <div className=" overflow-x-hidden absolute rounded-full w-[60vw] h-[60vw] bg-[#79AEEC66] bg-opacity-20 backdrop-blur-[80px] animate-float -z-10 top-[50vh] left-[-20vw]"></div>
+          <div className=" overflow-x-hidden absolute rounded-full w-[40vw] h-[40vw] bg-gradient-to-br from-[#63a878] via-[#5593c6] to-[#c5689d] bg-opacity-20 backdrop-blur-[80px] animate-float -z-10 top-[80vh] left-[-20vw]"></div>
+          <div className=" overflow-x-hidden absolute rounded-full w-[30vw] h-[30vw] bg-gradient-to-br from-[#A87863] via-[#9bc655] to-[#c56868] bg-opacity-20 backdrop-blur-[80px] animate-float -z-10 top-[150vh] right-[-10vw]"></div>{" "}
         </div>
         <div className="relative home overflow-x-hidden">
           <App />
