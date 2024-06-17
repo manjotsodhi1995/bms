aa15cc0dcec2c2ff8de570315d7ae5da60c4e9c5
Manjot Singh <manjotsodhi1995@gmail.com>
2024-06-17 15:37:04 +0530

    fixed build issue

--- src/components/HeroSlides.tsx
+++ src/components/HeroSlides.tsx
@@ -15,13 +15,13 @@ function HeroSlides({ title, description, imageUrl }: HeroSlidesProps) {
   };
   return (
     <div
-      className="md:rounded-3xl rounded-xl transition-transform duration-300 ease-in-out lg:h-[25rem] 2xl:h-[35rem] lg:w-[16rem] 2xl:w-[23rem]"
+      className="md:rounded-3xl rounded-xl transition-transform duration-300 ease-in-out lg:h-[18rem] 2xl:h-[25rem] lg:w-[12rem] 2xl:w-[16rem] h-[14rem]"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       style={{ transform: isHovered ? "scale(1.1)" : "scale(1)" }}
     >
       <div className="w-full rounded-3xl relative h-full">
-        <div className="absolute bg-white rounded-lg text-center ml-4 mt-4 p-1 font-medium text-[0.8rem] md:text-[1rem] lg:text-[1.2rem] 2xl:text-[1.5rem] 2xl:p-2">
+        <div className="absolute bg-white rounded-lg text-center ml-4 mt-4 p-1 font-medium text-[0.6rem] md:text-[0.8rem] lg:text-[1rem] 2xl:text-[1.2rem] 2xl:p-2">
           24 <br /> DEC
         </div>
         {/* <video
@@ -41,10 +41,10 @@ function HeroSlides({ title, description, imageUrl }: HeroSlidesProps) {
         />
 
         <div className="bg-[rgb(255,255,255,0.2)] backdrop-blur-2xl md:p-4 py-1 absolute bottom-[0rem] z-20 w-full flex flex-col gap-1 md:rounded-b-3xl rounded-b-xl">
-          <div className="flex justify-center font-medium text-[0.9rem] md:text-[0.9rem] lg:text-[1.6rem] 2xl:text-[2rem] text-white">
+          <div className="flex justify-center font-medium text-[0.9rem] md:text-[0.9rem] lg:text-[1rem] 2xl:text-[1.5rem] text-white">
             {title}
           </div>
-          <div className="text-white px-2 2xl:px-4 flex justify-center text-center text-[0.6rem] md:text-[0.9rem] 2xl:text-[1.4rem]">
+          <div className="text-white px-2 2xl:px-4 flex justify-center text-center text-[0.6rem] md:text-[0.6rem] 2xl:text-[1rem]">
             {description}
           </div>
         </div>
