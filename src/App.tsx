71f5efccb194819c863b713980512d4ee7b0e610
dead8309 <vaibhavmonu8309@gmail.com>
2024-06-23 03:27:20 +0530

    fmt files

--- src/App.tsx
+++ src/App.tsx
@@ -30,87 +30,87 @@ const Help = lazy(() => import("./pages/help"));
 const HelpDetail = lazy(() => import("./pages/help/helpDetail"));
 const Affiliate = lazy(() => import("./pages/Affiliate"));
 const PaymentsMethodPage = lazy(
-    () => import("./pages/dashboard/payout/payments-method"),
+  () => import("./pages/dashboard/payout/payments-method"),
 );
 const ProfilePage = lazy(() => import("./pages/dashboard/profile"));
 const EventsPage = lazy(() => import("./pages/dashboard/events"));
 
 const App = observer(() => {
-    const {
-        root: { auth },
-    } = useStore();
-    useEffect(() => {
-        const accessToken = localStorage.getItem("accessToken");
-        const refreshToken = localStorage.getItem("refreshToken");
-        if (accessToken && refreshToken) {
-            auth.accessToken = accessToken;
-            auth.refreshToken = refreshToken;
-            auth.setAuthenticated();
-        }
-    }, []);
-    return (
-        <div className="relative overflow-hidden">
-            <BrowserRouter>
-                <div className="home absolute -z-50">
-                    <Bg />
-                </div>
-                <div className="home min-h-full min-w-[100vw] -z-10 absolute"></div>
-                <div className="home min-h-full min-w-[100vw] -z-10 absolute"></div>
-                <div className="home min-h-full min-w-[100vw] -z-10 absolute"></div>
-
-                {<Navbar />}
-                <Suspense
-                    fallback={
-                        <div className="min-h-screen w-screen flex justify-center items-center">
-                            <img src={load} alt="" className="w-[10vw] h-[10vw]" />
-                        </div>
-                    }
-                >
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route
-                            path="/organization/:orgId"
-                            element={<OrganizationProfile />}
-                        />
-
-                        {/* Protected Routes */}
-                        <Route element={<ProtectedRoutes />}>
-                            <Route path="/mytickets" element={<MyTickets />} />
-                            <Route
-                                path="/organizer/register"
-                                element={<OrganizerRegister />}
-                            />
-                            <Route path="/organizer/login" element={<OrganizerLogin />} />
-                            <Route path="/dashboard" element={<DashBoard />} />
-                            <Route path="/dashboard/payout" element={<PayOutD />} />
-                            <Route
-                                path="/dashboard/payout/payments-method"
-                                element={<PaymentsMethodPage />}
-                            />
-                            <Route path="/dashboard/profile" element={<ProfilePage />} />
-                            <Route path="/dashboard/events" element={<EventsPage />} />
-                        </Route>
-
-                        {/* Public Routes */}
-                        <Route path="/search" element={<SearchPage />} />
-                        <Route path="/dashboard" element={<DashBoard />} />
-                        <Route path="/settings" element={<AccountSettings />} />
-                        <Route path="/filter" element={<Filter />} />
-                        <Route path="/terms" element={<TermsAndConditions />} />
-                        <Route path="/event/:eventId" element={<EventPage />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/contactus" element={<ContactUs />} />
-                        <Route path="/help" element={<Help />} />
-                        <Route path="/helpdetails" element={<HelpDetail />} />
-                        <Route path="/affiliate" element={<Affiliate />} />
-                        <Route path="/privacy" element={<Privacy />} />
-                        <Route path="/forgot" element={<Forgot />} />
-                        <Route path="/dashboard/payout" element={<PayOutD />} />
-                    </Routes>{" "}
-                </Suspense>
-            </BrowserRouter>
+  const {
+    root: { auth },
+  } = useStore();
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    const refreshToken = localStorage.getItem("refreshToken");
+    if (accessToken && refreshToken) {
+      auth.accessToken = accessToken;
+      auth.refreshToken = refreshToken;
+      auth.setAuthenticated();
+    }
+  }, []);
+  return (
+    <div className="relative overflow-hidden">
+      <BrowserRouter>
+        <div className="home absolute -z-50">
+          <Bg />
         </div>
-    );
+        <div className="home min-h-full min-w-[100vw] -z-10 absolute"></div>
+        <div className="home min-h-full min-w-[100vw] -z-10 absolute"></div>
+        <div className="home min-h-full min-w-[100vw] -z-10 absolute"></div>
+
+        {<Navbar />}
+        <Suspense
+          fallback={
+            <div className="min-h-screen w-screen flex justify-center items-center">
+              <img src={load} alt="" className="w-[10vw] h-[10vw]" />
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/organization/:orgId"
+              element={<OrganizationProfile />}
+            />
+
+            {/* Protected Routes */}
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/mytickets" element={<MyTickets />} />
+              <Route
+                path="/organizer/register"
+                element={<OrganizerRegister />}
+              />
+              <Route path="/organizer/login" element={<OrganizerLogin />} />
+              <Route path="/dashboard" element={<DashBoard />} />
+              <Route path="/dashboard/payout" element={<PayOutD />} />
+              <Route
+                path="/dashboard/payout/payments-method"
+                element={<PaymentsMethodPage />}
+              />
+              <Route path="/dashboard/profile" element={<ProfilePage />} />
+              <Route path="/dashboard/events" element={<EventsPage />} />
+            </Route>
+
+            {/* Public Routes */}
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/dashboard" element={<DashBoard />} />
+            <Route path="/settings" element={<AccountSettings />} />
+            <Route path="/filter" element={<Filter />} />
+            <Route path="/terms" element={<TermsAndConditions />} />
+            <Route path="/event/:eventId" element={<EventPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/helpdetails" element={<HelpDetail />} />
+            <Route path="/affiliate" element={<Affiliate />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/forgot" element={<Forgot />} />
+            <Route path="/dashboard/payout" element={<PayOutD />} />
+          </Routes>{" "}
+        </Suspense>
+      </BrowserRouter>
+    </div>
+  );
 });
 export default App;
