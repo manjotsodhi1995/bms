666e356995904de4b5ab0ae403e742a74b7d6055
dead8309 <vaibhavmonu8309@gmail.com>
2024-07-02 22:19:14 +0530

    fix event strory height

--- src/components/eventStory.tsx
+++ src/components/eventStory.tsx
@@ -10,25 +10,23 @@ function EventSlides({ title, description, posterUrl: imageUrl }: EventType) {
   };
   return (
     <div
-      className="h-full md:rounded-3xl rounded-xl transition-transform duration-300 ease-in-out"
+      className="h-[30vh] md:rounded-3xl rounded-xl transition-transform duration-300 ease-in-out relative"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       style={{ transform: isHovered ? "scale(1.1)" : "scale(1)" }}
     >
-      <div className="w-full rounded-3xl relative h-full">
-        <img
-          src={imageUrl}
-          alt=""
-          className="w-full md:rounded-3xl rounded-xl h-full"
-          onLoad={handleImageLoad}
-        />
-        <div className="bg-[rgb(255,255,255,0.2)] backdrop-blur-2xl md:p-2 py-1 absolute bottom-[0rem] z-20 w-full flex flex-col md:rounded-b-3xl rounded-b-xl border b-1">
-          <div className="font-medium text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] 2xl:text-[1.2rem] text-white line-clamp-1 break-words">
-            {title}
-          </div>
-          <div className="text-white px-2 text-center text-[0.4rem] md:text-[0.6rem] 2xl:text-[0.7rem] line-clamp-1">
-            {description}
-          </div>
+      <img
+        src={imageUrl}
+        alt=""
+        className="w-full md:rounded-3xl rounded-xl h-full object-cover"
+        onLoad={handleImageLoad}
+      />
+      <div className="bg-[rgb(255,255,255,0.2)] backdrop-blur-2xl md:p-2 py-1 absolute bottom-[0rem] z-20 w-full flex flex-col md:rounded-b-3xl rounded-b-xl border b-1">
+        <div className="font-medium text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] 2xl:text-[1.2rem] text-white line-clamp-1 break-words">
+          {title}
+        </div>
+        <div className="text-white px-2 text-center text-[0.4rem] md:text-[0.6rem] 2xl:text-[0.7rem] line-clamp-1">
+          {description}
         </div>
       </div>
     </div>
