dfc570b815cb8e0de66d2800f8596b739b2247ea
dead8309 <vaibhavmonu8309@gmail.com>
2024-07-19 02:48:28 +0530

    add back button in payment success step

--- src/components/ticket/steps/PaymentSuccessStep.tsx
+++ src/components/ticket/steps/PaymentSuccessStep.tsx
@@ -1,6 +1,7 @@
 import { EventType } from "@/stores/event";
 import { TicketStepsProps } from ".";
 import EventCard from "@/components/EventCard";
+import { ChevronLeft } from "lucide-react";
 
 const eventData = [
   {
@@ -29,48 +30,56 @@ const eventData = [
   },
 ] satisfies Partial<EventType>[];
 
-export const PaymentSuccessStep = ({ onStepChange }: TicketStepsProps) => {
+export const PaymentSuccessStep = ({
+  onStepChange,
+  onBack,
+}: TicketStepsProps) => {
   return (
-    <div className="w-full flex flex-col items-center justify-center gap-10">
-      <div className="flex flex-col items-center justify-center gap-10">
-        <span className="text-xl font-semibold">Payment successfull!</span>
+    <>
+      <button onClick={onBack}>
+        <ChevronLeft className="size-6" />
+      </button>
+      <div className="w-full flex flex-col items-center justify-center gap-10">
+        <div className="flex flex-col items-center justify-center gap-10">
+          <span className="text-xl font-semibold">Payment successfull!</span>
 
-        <div className="mt-10">
-          <button
-            className="mt-4 bg-black w-full text-white font-medium py-2 rounded-md"
-            onClick={onStepChange}
-          >
-            View Ticket
-          </button>
+          <div className="mt-10">
+            <button
+              className="mt-4 bg-black w-full text-white font-medium py-2 rounded-md"
+              onClick={onStepChange}
+            >
+              View Ticket
+            </button>
 
-          <p className="underline text-sm mt-2">
-            A ticket conformation has been sent to your email
-          </p>
+            <p className="underline text-sm mt-2">
+              A ticket conformation has been sent to your email
+            </p>
+          </div>
+
+          <div className="flex w-full gap-4 font-thin text-sm">
+            <button className="mt-4 bg-black w-full text-white font-medium py-2 rounded-md">
+              Add to groupchat
+            </button>
+            <button className="mt-4 bg-black w-full text-white font-medium py-2 rounded-md">
+              Rep this event
+            </button>
+          </div>
         </div>
 
-        <div className="flex w-full gap-4 font-thin text-sm">
-          <button className="mt-4 bg-black w-full text-white font-medium py-2 rounded-md">
-            Add to groupchat
-          </button>
-          <button className="mt-4 bg-black w-full text-white font-medium py-2 rounded-md">
-            Rep this event
-          </button>
-        </div>
-      </div>
+        <div className="lg:px-[5%] xl:px-[7%] px-[8vw] py-[2rem] flex w-full">
+          <div className="flex flex-col gap-4 w-full">
+            <span className="font-medium lg:text-[1.4rem] text-[0.9rem] mt-4">
+              Find more events
+            </span>
 
-      <div className="lg:px-[5%] xl:px-[7%] px-[8vw] py-[2rem] flex w-full">
-        <div className="flex flex-col gap-4 w-full">
-          <span className="font-medium lg:text-[1.4rem] text-[0.9rem] mt-4">
-            Find more events
-          </span>
-
-          <div className="flex gap-2 justify-around">
-            {eventData.slice(0, 2).map((card, index) => (
-              <EventCard key={index} {...card} />
-            ))}
+            <div className="flex gap-2 justify-around">
+              {eventData.slice(0, 2).map((card, index) => (
+                <EventCard key={index} {...card} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
