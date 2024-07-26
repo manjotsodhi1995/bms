22029fc9ea61c6eb69179564fab07dce2004fc36
dead8309 <vaibhavmonu8309@gmail.com>
2024-07-26 23:27:44 +0530

    add close button for booking dialog

--- src/components/ticket/BookTicketsDialog.tsx
+++ src/components/ticket/BookTicketsDialog.tsx
@@ -1,5 +1,10 @@
 import { DialogProps } from "@radix-ui/react-dialog";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { useState } from "react";
 import { observer } from "mobx-react-lite";
 import { TicketStep } from "@/stores/ticket";
@@ -13,6 +18,7 @@ import { BuyAnotherTicketsStep } from "./steps/BuyAnotherTicketStep";
 import { ShareTicketStep } from "./steps/ShareTicketStep";
 import type { EventType } from "@/stores/event";
 import CartProvider from "@/stores/cart";
+import { X } from "lucide-react";
 
 interface BookTicketsDialogProps extends DialogProps {
   eventsData: EventType | null;
@@ -29,6 +35,11 @@ const BookTicketsDialog = observer(
           <DialogTrigger asChild>{children}</DialogTrigger>
           <DialogContent className="overflow-y-auto p-1 max-w-screen-lg max-h-[calc(100dvh)]">
             <div className=" p-12 backdrop-blur-md">
+              <DialogClose className="absolute right-4 top-4 opacity-70 rounded-sm ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+                <X className="size-6" />
+                <span className="sr-only">Close</span>
+              </DialogClose>
+
               {ticketStep === "booking" && (
                 <BookingStep
                   onBack={() => setDialogOpen(false)}
