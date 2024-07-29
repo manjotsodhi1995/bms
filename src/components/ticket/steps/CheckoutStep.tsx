4ff57f714a9e616b0a6285277eb8c0953f015811
dead8309 <vaibhavmonu8309@gmail.com>
2024-07-29 19:51:47 +0530

    left align fees text

--- src/components/ticket/steps/CheckoutStep.tsx
+++ src/components/ticket/steps/CheckoutStep.tsx
@@ -181,7 +181,7 @@ export const CheckoutStep = ({
             </p>
             <p className="flex items-center justify-between text-gray-600 text-sm">
               <span>Fees</span>
-              <span>
+              <span className="mr-[1.6rem]">
                 ${cartData ? cartData.totalAmount - cartData.subTotal : "0.00"}
               </span>
             </p>
