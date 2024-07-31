8640d8e087b36480952a0bc67593b2980502bbfe
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2024-08-01 02:05:09 +0530

    title

--- src/pages/CategoriesPage.tsx
+++ src/pages/CategoriesPage.tsx
@@ -47,9 +47,10 @@ function CategoriesPage() {
           </div>
         </div>
         <div className="justify-center w-full grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-4">
-          {data && data.upcomingEvents.map((card: any, index: number) => (
-            <EventCard key={index} {...card} />
-          ))}
+          {data &&
+            data.upcomingEvents.map((card: any, index: number) => (
+              <EventCard key={index} {...card} />
+            ))}
         </div>
       </div>
     </div>
