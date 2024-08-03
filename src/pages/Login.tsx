b07dd0098b7fcf60d415681cbddeb4114696d99e
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-08-03 21:25:58 +0530

    fix login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -73,82 +73,76 @@ const Login = observer(() => {
           <img src={img} className="h-screen w-full" alt="" />
         </div>
         <div className="h-full flex justify-center w-full">
-          <div className="h-full flex justify-center w-full">
-            <form
-              onSubmit={handleSubmit}
-              className="flex flex-col justify-center gap-6 w-[25rem] md:w-[50%]"
-            >
-              <div className="flex flex-col gap-2 items-center">
-                <h1 className="text-3xl font-bold">Login</h1>
-                {/* <h6 className="text-md">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col justify-center gap-4"
+          >
+            <div className="flex flex-col gap-2 items-center">
+              <h1 className="text-3xl font-bold">Login</h1>
+              {/* <h6 className="text-md">
                   Please enter your login details to sign in
                 </h6> */}
+            </div>
+            <div className="flex flex-col gap-4 w-full justify-center items-center">
+              <input
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email Address"
+                id="email"
+                className="w-[100%]"
+                onChange={handleInputChange}
+              />
+              <input
+                type="password"
+                value={password}
+                placeholder="Password"
+                name="password"
+                id="password"
+                className="w-[100%]"
+                onChange={handleInputChange}
+              />
+              <div className="text-[#8C3E87] text-end hover:text-[#763572] font-bold place-self-end">
+                <Link to="/forgot">Forgot Password ?</Link>
               </div>
-              <div className="flex flex-col gap-4">
-                <div className="">
-                  <input
-                    type="email"
-                    name="email"
-                    value={email}
-                    placeholder="Email Address"
-                    id="email"
-                    className="w-full"
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <div className="flex flex-col gap-1">
-                  <input
-                    type="password"
-                    value={password}
-                    placeholder="Password"
-                    name="password"
-                    id="password"
-                    className="w-full"
-                    onChange={handleInputChange}
-                  />
-                  <div className="text-[#8C3E87] text-end hover:text-[#763572] font-bold">
-                    <Link to="/forgot">Forgot Password ?</Link>
-                  </div>
-                </div>
-              </div>
+            </div>
 
-              <div className="w-full text-[1rem] flex flex-col gap-1 items-center">
-                <button
-                  type="submit"
-                  className="flex items-center justify-center gap-4 bg-black w-full text-white font-bold py-2 px-4 rounded"
-                >
-                  Sign In
-                  {loading && <Loader2 className="size-4 animate-spin" />}
-                </button>
-                <div className="text-red-600">{error}</div>
-              </div>
-              <div className="w-full items-center flex justify-center">
-                <GoogleLogin
-                  onSuccess={responseGoogle}
-                  size="large"
-                  width={"380px"}
-                />
-              </div>
-              <div className="flex w-full justify-around gap-2 items-center">
-                <div className="h-[2px] w-full bg-black"></div>
-                <div className="text-center">or</div>
-                <div className="w-full h-[2px] bg-black"></div>
-              </div>
+            <div className="text-[1rem] w-full flex flex-col gap-1 justify-center items-center">
+              <button
+                type="submit"
+                className="flex w-[100%] items-center justify-center gap-4 bg-black text-white font-bold py-2 px-4 rounded"
+              >
+                Sign In
+                {loading && <Loader2 className="size-4 animate-spin" />}
+              </button>
+              <div className="text-red-600">{error}</div>
+            </div>
+            <div className="w-full items-center flex justify-center">
+              <GoogleLogin
+                onSuccess={responseGoogle}
+                size="large"
+                width="330px"
+              />
+            </div>
+            <div className="flex w-full justify-around gap-2 items-center">
+              <div className="h-[2px] w-full bg-black"></div>
+              <div className="text-center">or</div>
+              <div className="w-full h-[2px] bg-black"></div>
+            </div>
 
-              <div className="text-[0.9rem] flex justify-center gap-1 font-semibold">
-                <Link to="/register" className="text-[#8C3E87]">
-                  {" "}
-                  Create an account
-                </Link>
-              </div>
-              {/* <div className="flex flex-col gap-4">
+            <div className="text-[0.9rem] flex justify-center gap-1 font-semibold">
+              <Link to="/register" className="text-[#8C3E87]">
+                {" "}
+                Create an account
+              </Link>
+            </div>
+            {/* <div className="flex flex-col gap-4">
                 <div className="h-[2px] bg-black"></div>
                 <button className="bg-black w-full text-white font-semibold py-2 px-4 rounded">
                   <Link to="/organizer/login">I'm an organizer</Link>
                 </button>
               </div> */}
-            </form>
-          </div>
+          </form>
         </div>
       </div>
     </>
