21b8b148af240c5a7ed424fdfc7e999222beda6f
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-08-05 20:49:34 +0530

    update profile page

--- src/pages/AccountSettings.tsx
+++ src/pages/AccountSettings.tsx
@@ -2,24 +2,63 @@ import SideBar from "@/components/accountsettings/SideBar";
 import AccSettings from "@/components/accountsettings/AccSettings";
 import PaymentCard from "@/components/accountsettings/PaymentCard";
 import PaymentMethod from "@/components/accountsettings/PaymentMethod";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-interface AccountSettings {
+interface AccountSettingsProps {
   defaultSettingState: string;
 }
 
-const AccountSettings: React.FC<AccountSettings> = ({defaultSettingState}) => {
+const AccountSettings: React.FC<AccountSettingsProps> = ({
+  defaultSettingState,
+}) => {
   const [sidebarState, setSidebarState] = useState<string>(defaultSettingState);
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+  const [isManuallyToggled, setIsManuallyToggled] = useState(false);
+
   const handleStateChange = (newState: string) => {
     setSidebarState(newState);
-  }
+  };
+
+  const toggleSidebar = () => {
+    setSidebarVisible((prev) => !prev);
+    setIsManuallyToggled(true);
+    console.log('Sidebar toggled:', !isSidebarVisible); // Debugging log
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth >= 768) {
+      setSidebarVisible(true);
+      setIsManuallyToggled(true);
+    } else if (!isManuallyToggled) {
+      setSidebarVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [isManuallyToggled]);
 
   return (
-    <div className="flex flex-col sm:flex-row select-none w-screen items-center h-fit">
-        <SideBar onStateChange={handleStateChange} initialState={sidebarState}/>
-        {sidebarState === "acc"? <AccSettings/>:sidebarState === "pay"? <PaymentMethod/>:sidebarState === "paycard"? <PaymentCard/>: "" }
+    <div className="md:flex select-none w-screen items-center">
+      <SideBar
+        onStateChange={handleStateChange}
+        initialState={sidebarState}
+        isVisible={isSidebarVisible}
+      />
+      {sidebarState === "acc" ? (
+        <AccSettings toggleSidebar={toggleSidebar} isVisible={isSidebarVisible}
+        />
+      ) : sidebarState === "pay" ? (
+        <PaymentMethod />
+      ) : sidebarState === "paycard" ? (
+        <PaymentCard />
+      ) : (
+        ""
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default AccountSettings
+export default AccountSettings;
