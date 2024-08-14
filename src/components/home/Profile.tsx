33bca391cf82f96a8bc30cf9896d932a531b7050
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2024-08-14 23:51:19 +0530

    minor fixes

--- src/components/home/Profile.tsx
+++ src/components/home/Profile.tsx
@@ -1,7 +1,7 @@
 import img from "../../assets/affiliate/aff14.jpg";
 function Profile() {
   return (
-    <div className="mt-20 w-full back object-cover">
+    <div className="mt-20 w-full back object-cover relative">
       <img src={img} alt="" />
     </div>
   );
