80651913004d169b4cc5596c52d30bc59f774c54
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-08-15 21:07:50 +0530

    integrated filter api

--- src/pages/Filter.tsx
+++ src/pages/Filter.tsx
@@ -7,6 +7,11 @@ import { useEffect, useState } from "react";
 import "react-calendar/dist/Calendar.css";
 import axios from "@/utils/middleware";
 
+interface DayResult {
+  liveEvents: Event[];
+  upcomingEvents: Event[];
+}
+
 function Filter() {
   const {
     root: { event },
@@ -16,65 +21,60 @@ function Filter() {
   };
 
   const [filters, setFilters] = useState<any>({
-    day: {
-      Today: false,
-      Tomorrow: false,
-      ThisWeek: false,
-      ThisMonth: false,
-    },
-    pricing: {
-      Free: false,
-      Paid: false,
-    },
+    day: [],
+    pricing: [],
+    category: [],
   });
-  const [dayResult, setDayResult] = useState<any[]>([]);
-  const handleFilterChange = (
-    category: string,
-    option: string,
-    isChecked: boolean
-  ) => {
-    setFilters((prevFilters: any) => ({
-      ...prevFilters,
-      [category]: {
-        ...prevFilters[category],
-        [option]: isChecked,
-      },
-    }));
+
+  const [dayResult, setDayResult] = useState<DayResult>({
+    liveEvents: [],
+    upcomingEvents: [],
+  });
+
+  const [categories, setCategories] = useState<any[]>([]);
+
+  const fetchCategory = async () => {
+    const res = await axios.get(
+      `https://kafsbackend-106f.onrender.com/api/v1/events/fetch?city=Dublin`
+    );
+    setCategories(res.data.data.trendingCategories);
   };
 
   const fetchData = async () => {
-    const selectedDays = [];
-    if (filters.day.Today) selectedDays.push("Today");
-    if (filters.day.Tomorrow) selectedDays.push("Tomorrow");
-    if (filters.day.ThisWeek) selectedDays.push("ThisWeek");
-    if (filters.day.ThisMonth) selectedDays.push("ThisMonth");
-
     const dayParam =
-      selectedDays.length > 0 ? `day=${selectedDays.join("&day=")}` : "";
-
-    const pricingParam = [];
-    if (filters.pricing.free) pricingParam.push("free");
-    if (filters.pricing.paid) pricingParam.push("paid");
+      filters.day.length > 0 ? `day=${filters.day.join("&day=")}` : "";
 
     const pricingQuery =
-      pricingParam.length > 0
-        ? `pricing=${pricingParam.join("&pricing=")}`
+      filters.pricing.length > 0
+        ? `pricing=${filters.pricing.join("&pricing=")}`
+        : "";
+
+    const categoryQuery =
+      filters.category.length > 0
+        ? `category=${filters.category.join("&category=")}`
         : "";
 
     try {
       const response = await axios.get(
         `https://kafsbackend-106f.onrender.com/api/v1/events/fetch?city=Dublin${
           dayParam ? `&${dayParam}` : ""
-        }${pricingQuery ? `&${pricingQuery}` : ""}`
+        }${pricingQuery ? `&${pricingQuery}` : ""}${
+          categoryQuery ? `&${categoryQuery}` : ""
+        }`
       );
 
-      setDayResult(response.data.data.liveEvents);
+      setDayResult({
+        liveEvents: response.data.data.liveEvents,
+        upcomingEvents: response.data.data.upcomingEvents,
+      });
     } catch (error) {
       console.log("Error while fetching", error);
     }
   };
 
-  // Fetch data when filters change
+  useEffect(() => {
+    fetchCategory();
+  }, []);
   useEffect(() => {
     fetchData();
   }, [filters]);
@@ -89,10 +89,8 @@ function Filter() {
 
   return (
     <div className="lg:pr-[5%] xl:pr-[7%] pr-[0vw] flex md:gap-2 2xl:gap-10 flex-col sm:flex-row text-lg">
-      {/* <div className="bg-red-400 min-h-4 min-w-4 p-24 rounded-full absolute"></div> */}
       <div className="z-10 md:w-[25rem] w-[100vw] flex flex-col mt-[50px] pt-[40px] h-[1005] rounded-r-xl backdrop-blur-2xl ml-1 pl-2 bg-white/10 border-r border-t border-b border-white/20 shadow-lg">
-      {" "}
-        {/* Fixed positioning and overflow */}
+        {" "}
         <div className="border-gray-400 border-b mb-[30px] pl-[30px] ">
           <h2 className="text-xl font-medium mt-[10px] mb-[20px]">
             Filter By:
@@ -110,12 +108,19 @@ function Filter() {
             <div className="flex">
               <input
                 type="checkbox"
-                name=""
-                id=""
-                checked={filters.day.Today}
-                onChange={(e) =>
-                  handleFilterChange("day", "Today", e.target.checked)
-                }
+                checked={filters.day.includes("Today")}
+                onChange={(e) => {
+                  setFilters((prev: any) => {
+                    const updatedDay = e.target.checked
+                      ? [...prev.day, "Today"]
+                      : prev.day.filter((day: any) => day !== "Today");
+
+                    return {
+                      ...prev,
+                      day: updatedDay,
+                    };
+                  });
+                }}
                 className="mr-[10px] w-[17px] border-black"
               />
               <h2>Today</h2>
@@ -125,12 +130,19 @@ function Filter() {
             <div className="flex">
               <input
                 type="checkbox"
-                name=""
-                id=""
-                checked={filters.day.Tomorrow}
-                onChange={(e) =>
-                  handleFilterChange("day", "Tomorrow", e.target.checked)
-                }
+                checked={filters.day.includes("Tomorrow")}
+                onChange={(e) => {
+                  setFilters((prev: any) => {
+                    const updatedDay = e.target.checked
+                      ? [...prev.day, "Tomorrow"]
+                      : prev.day.filter((day: any) => day !== "Tomorrow");
+
+                    return {
+                      ...prev,
+                      day: updatedDay,
+                    };
+                  });
+                }}
                 className="mr-[10px] w-[17px] border-black"
               />
               <h2>Tommorow</h2>
@@ -140,12 +152,19 @@ function Filter() {
             <div className="flex">
               <input
                 type="checkbox"
-                name=""
-                id=""
-                checked={filters.day.ThisWeek}
-                onChange={(e) =>
-                  handleFilterChange("day", "ThisWeek", e.target.checked)
-                }
+                checked={filters.day.includes("ThisWeek")}
+                onChange={(e) => {
+                  setFilters((prev: any) => {
+                    const updatedDay = e.target.checked
+                      ? [...prev.day, "ThisWeek"]
+                      : prev.day.filter((day: any) => day !== "ThisWeek");
+
+                    return {
+                      ...prev,
+                      day: updatedDay,
+                    };
+                  });
+                }}
                 className="mr-[10px] w-[17px] border-black"
               />
               <h2>This Week</h2>
@@ -155,12 +174,19 @@ function Filter() {
             <div className="flex mb-[20px]">
               <input
                 type="checkbox"
-                name=""
-                id=""
-                checked={filters.day.ThisMonth}
-                onChange={(e) =>
-                  handleFilterChange("day", "ThisMonth", e.target.checked)
-                }
+                checked={filters.day.includes("ThisMonth")}
+                onChange={(e) => {
+                  setFilters((prev: any) => {
+                    const updatedDay = e.target.checked
+                      ? [...prev.day, "ThisMonth"]
+                      : prev.day.filter((day: any) => day !== "ThisMonth");
+
+                    return {
+                      ...prev,
+                      day: updatedDay,
+                    };
+                  });
+                }}
                 className="mr-[10px] w-[17px] border-black"
               />
               <h2>This Month</h2>
@@ -173,12 +199,19 @@ function Filter() {
             <div className="flex">
               <input
                 type="checkbox"
-                name=""
-                id=""
-                checked={filters.pricing.free}
-                onChange={(e) =>
-                  handleFilterChange("pricing", "free", e.target.checked)
-                }
+                checked={filters.pricing.includes("Free")}
+                onChange={(e) => {
+                  setFilters((prev: any) => {
+                    const updatedPricing = e.target.checked
+                      ? [...prev.pricing, "Free"]
+                      : prev.pricing.filter((day: any) => day !== "Free");
+
+                    return {
+                      ...prev,
+                      pricing: updatedPricing,
+                    };
+                  });
+                }}
                 className="mr-[10px] w-[17px] border-black"
               />
               <h2>Free</h2>
@@ -188,12 +221,19 @@ function Filter() {
             <div className="flex mb-[20px]">
               <input
                 type="checkbox"
-                name=""
-                id=""
-                checked={filters.pricing.paid}
-                onChange={(e) =>
-                  handleFilterChange("pricing", "paid", e.target.checked)
-                }
+                checked={filters.pricing.includes("Paid")}
+                onChange={(e) => {
+                  setFilters((prev: any) => {
+                    const updatedPricing = e.target.checked
+                      ? [...prev.pricing, "Paid"]
+                      : prev.pricing.filter((day: any) => day !== "Paid");
+
+                    return {
+                      ...prev,
+                      pricing: updatedPricing,
+                    };
+                  });
+                }}
                 className="mr-[10px] w-[17px] border-black"
               />
               <h2>Paid</h2>
@@ -202,162 +242,57 @@ function Filter() {
         </div>
         <div className="border-gray-400 border-b space-y-3 mb-[50px] pl-[30px]">
           <h3 className="font-medium mb-[20px] text-xl">Categories</h3>
-          <div className="max-sm:text-sm flex justify-between w-[200px]">
-            <div className="flex">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Music</h2>
-            </div>
-          </div>
-          <div className="max-sm:text-sm flex justify-between w-[200px]">
-            <div className="flex">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Comedy</h2>
-            </div>
-          </div>
-          <div className="max-sm:text-sm flex justify-between w-[200px]">
-            <div className="flex">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Sports</h2>
-            </div>
-          </div>
-          <div className="max-sm:text-sm flex justify-between w-[200px]">
-            <div className="flex">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Food & Drinks</h2>
-            </div>
-          </div>
-          <div className="max-sm:text-sm flex justify-between w-[200px]">
-            <div className="flex">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Movie</h2>
-            </div>
-          </div>
-          <div className="max-sm:text-sm flex justify-between w-[200px]">
-            <div className="flex">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Workshops</h2>
-            </div>
-          </div>
-          <div className="max-sm:text-sm flex justify-between w-[200px]">
-            <div className="flex">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Dance</h2>
-            </div>
-          </div>
-          <div className="max-sm:text-sm flex justify-between w-[200px]">
-            <div className="flex">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Art</h2>
-            </div>
-          </div>
-          <div className="max-sm:text-sm flex justify-between w-[200px]">
-            <div className="flex mb-[20px]">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Other</h2>
-            </div>
-          </div>
-        </div>
-        <div className="mb-[100px] space-y-3 pl-[30px]">
-          <h3 className="font-medium mb-[20px]">Near By Cities</h3>
-          <div className="flex justify-between w-[200px]">
-            <div className="flex">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Ahmedabad</h2>
-            </div>
-          </div>
-          <div className="flex justify-between w-[200px]">
-            <div className="flex">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Gandhinagar</h2>
-            </div>
-          </div>
-          <div className="flex justify-between w-[200px]">
-            <div className="flex">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                className="mr-[10px] w-[17px] border-black"
-              />
-              <h2>Surat</h2>
-            </div>
+          <div className="max-sm:text-sm flex space-y-3 flex-col justify-between w-[200px]">
+            {categories.map((data: any) => (
+              <div className="flex" key={data._id}>
+                <input
+                  type="checkbox"
+                  value={data._id}
+                  onChange={(e) => {
+                    setFilters((prev: any) => {
+                      const updatedCategory = e.target.checked
+                        ? [...prev.category, e.target.value]
+                        : prev.category.filter(
+                            (id: any) => id !== e.target.value
+                          );
+
+                      return {
+                        ...prev,
+                        category: updatedCategory,
+                      };
+                    });
+                  }}
+                  className="mr-[10px] w-[17px] border-black"
+                />
+                <h2>{data.categoryName}</h2>
+              </div>
+            ))}
           </div>
         </div>
       </div>
 
       <div className="flex flex-col gap-6 w-full ml-4 mt-14">
         {" "}
-        {/* Adjusted margin-left */}
         <div className="flex justify-center sm:justify-normal">
           <div className="font-medium lg:text-[1.4rem] text-[0.9rem]">
             Events
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-12">
-          {dayResult.length > 0 ? (
-            dayResult.map((event, index) => (
-              <EventCard key={index} {...event} />
-            ))
+          {dayResult.liveEvents.length > 0 ||
+          dayResult.upcomingEvents.length > 0 ? (
+            <>
+              {dayResult.liveEvents.map((card, index) => (
+                <EventCard key={index} {...card} />
+              ))}
+              {dayResult.upcomingEvents.map((card, index) => (
+                <EventCard key={index} {...card} />
+              ))}
+            </>
           ) : (
             <div>No events found</div>
           )}
         </div>
-        
       </div>
     </div>
   );
