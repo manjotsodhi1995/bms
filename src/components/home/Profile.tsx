49f74e9293b9d8c19289f4ead921ea4d00a864d4
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-08-17 09:35:05 +0530

    added image

--- src/components/home/Profile.tsx
+++ src/components/home/Profile.tsx
@@ -1,8 +1,14 @@
-import img from "../../assets/affiliate/aff14.jpg";
 function Profile() {
   return (
-    <div className="mt-20 w-full back object-cover relative">
-      <img src={img} alt="" />
+    <div className="mt-20 w-full">
+      <div className="relative">
+        <img src={"affiliation section 2-05.jpg"} alt="" className="w-full" />
+        <div className="absolute bottom-16 left-1/4 transform -translate-x-1/2 mb-10">
+          <button className="bg-white text-[#102e38] py-3 px-8 rounded-full font-medium text-3xl">
+            Partner with Us
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
