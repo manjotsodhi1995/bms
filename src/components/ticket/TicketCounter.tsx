efd38223b25255b73b8ff908dccc8983895fa5da
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2024-08-22 22:42:04 +0530

    push

--- src/components/ticket/TicketCounter.tsx
+++ src/components/ticket/TicketCounter.tsx
@@ -11,24 +11,28 @@ interface TicketCounterProps {
 const TicketCounter = ({
   className,
   onValueChange,
+  value = 0,
   pending = false,
 }: TicketCounterProps) => {
-  const [value, setValue] = useState(0);
-  const [minus, setMinus] = useState(true);
+  const [value1, setValue] = useState(0);
+  const [minus, setMinus] = useState(false);
   useEffect(() => {
-    if (value == 0) setMinus(true);
-    else if (value > 0) setMinus(false);
-  }, [value]);
+    setValue(value);
+  }, []);
+  useEffect(() => {
+    if (value1 == 0) setMinus(true);
+    else if (value1 > 0) setMinus(false);
+  }, [value1]);
   // const [ticket, setTicket] = useState(value);
   const increaseTicket = () => {
     // setTicket((p) => p + 1);
-    setValue(value + 1);
-    onValueChange(value + 1);
+    setValue(value1 + 1);
+    onValueChange(value1 + 1);
   };
   const decreaseTicket = () => {
     // setTicket((p) => (p > 0 ? p - 1 : p));
-    setValue(value > 0 ? value - 1 : value);
-    onValueChange(value - 1);
+    setValue(value1 > 0 ? value1 - 1 : value1);
+    onValueChange(value1 - 1);
   };
   return (
     <div className={cn("flex items-center gap-4 mr-6", className)}>
@@ -40,7 +44,7 @@ const TicketCounter = ({
         <Minus className="size-5 stroke-1" />
       </button>
 
-      <span>{value}</span>
+      <span>{value1 == 0 ? value : value1}</span>
 
       <button
         disabled={pending}
