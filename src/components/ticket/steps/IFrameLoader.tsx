e395a59230b9e3293bc0d5d765262bb2e88f234b
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2024-08-29 01:24:59 +0530

    ok

--- src/components/ticket/steps/IFrameLoader.tsx
+++ src/components/ticket/steps/IFrameLoader.tsx
@@ -6,19 +6,9 @@ interface IFrameLoaderProps extends TicketStepsProps {
 }
 
 function IFrameLoader({ data }: IFrameLoaderProps) {
-  const hiddenInputValue =
-    "eyJ0aHJlZURTTWV0aG9kTm90aWZpY2F0aW9uVVJMIjoiaHR0cHM6Ly9rYWZzYmFja2VuZC0xMDZmLm9ucmVuZGVyLmNvbS9hcGkvdjEvcGF5bWVudHMvZGlyZWN0M2RzP3RocmVlRFNBY3NSZXNwb25zZT1tZXRob2QiLCJ0aHJlZURTU2VydmVyVHJhbnNJRCI6ImNiZDliNmRkLTlmNDItNGUzYy1hZDk1LTMyYTY5OTkzNGRmNCJ9"; // Update with actual value
+  console.log(data);
 
-  return (
-    <form action="https://acs.3ds-pit.com/?method" method="POST">
-      <input
-        type="hidden"
-        name="threeDSMethodData"
-        value={hiddenInputValue} // Set the hidden input value
-      />
-      <input type="submit" value="Submit" />
-    </form>
-  );
+  return <></>;
 }
 
 export default IFrameLoader;
