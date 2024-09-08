a6763330d7970cff969be7cdbdcd3bc7a717da01
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2024-09-08 23:55:45 +0530

    updates

--- src/pages/payment.tsx
+++ src/pages/payment.tsx
@@ -1,29 +1,19 @@
 import { CircleAlert, CircleCheck } from "lucide-react";
-import { useEffect, useState } from "react";
-import { Loader2 } from "lucide-react";
 import { useLocation } from "react-router-dom";
-
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
 
-
-
-
-function Payment({ setDialogOpen }: any) {
-  const [payment, setPayment] = useState("loading");
-
+function Payment({ setDialogOpen, dialogOpen }: any) {
   const query = useQuery();
-  const paymentStatus = query.get('status');
+  const paymentStatus = query.get("status");
+  console.log(dialogOpen + "dialog");
 
-  //   setPayment("success");
-  useEffect(() => {
-    setPayment("success");
-    setTimeout(setDialogOpen(false), 3000);
-  }, []);
+  setDialogOpen(false);
+
   return (
-    <div>
+    <div className="mt-0 pt-20">
       {" "}
       {paymentStatus === "succesful" && (
         <div className="w-full flex flex-col items-center justify-center gap-10 mt-4">
@@ -32,10 +22,9 @@ function Payment({ setDialogOpen }: any) {
             <CircleCheck className="size-32 text-green-500" />
 
             <div className="mt-2">
-              <p className="underline text-sm mt-2">
-                Thanks for choosing Kafsco. Check My Tickets Page to see your
-                tickets. A ticket confirmation has been sent to your email as
-                well!
+              <p className="underline text-sm mt-2 text-center">
+                Check My Tickets Page to see your tickets.
+                <br /> A ticket confirmation has been sent to your email!
               </p>
             </div>
 
@@ -60,29 +49,6 @@ function Payment({ setDialogOpen }: any) {
               <p className="text-sm mt-2">
                 Payment has failed due to some error. Please try again!
               </p>
-            </div>
-
-            {/* <div className="flex w-full gap-4 font-thin text-sm"> */}
-            {/*   <button className="mt-4 bg-black w-full text-white font-medium py-2 rounded-md"> */}
-            {/*     Add to groupchat */}
-            {/*   </button> */}
-            {/*   <button className="mt-4 bg-black w-full text-white font-medium py-2 rounded-md"> */}
-            {/*     Rep this event */}
-            {/*   </button> */}
-            {/* </div> */}
-          </div>
-        </div>
-      )}
-      {payment === "loading" && (
-        <div className="w-full flex flex-col items-center justify-center gap-10 mt-4">
-          <div className="flex flex-col items-center justify-center gap-10">
-            <span className="text-2xl font-semibold">
-              Processing Payment...
-            </span>
-
-            <div className="mt-2 flex flex-col justify-center items-center">
-              <Loader2 size={100} className="animate-spin" />
-              <p className="text-sm mt-8">Please wait for some moment..</p>
             </div>
 
             {/* <div className="flex w-full gap-4 font-thin text-sm"> */}
