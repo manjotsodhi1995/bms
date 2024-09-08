694c8aa234227a952c18c8497fa1ce7ab5650f94
Sanket Agrawal <sanket@ataloud.com>
2024-09-08 17:56:46 +0530

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 export const SERVER_BASE_URL = `${
   import.meta.env.VITE_BACKEND_SERVER_URL ||
-  "https://kafsbackend-106f.onrender.com"
+  "https://l2rdr7x3-3002.inc1.devtunnels.ms"
 }/api/v1` as const;
 
 export const API = {
