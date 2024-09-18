19167360c7e93c56b7e9a14f50f4769d76d0a60a
Adway <66119557+adway7103@users.noreply.github.com>
2024-09-18 22:11:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const Dotenv = require("dotenv-webpack");
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), new Dotenv()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
