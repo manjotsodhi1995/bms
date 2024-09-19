ad2b9b1c1e1da5b2de0d29ccbbde23ed41dd8780
Adway <66119557+adway7103@users.noreply.github.com>
2024-09-19 20:47:57 +0530

    Update CheckoutStep.tsx

--- src/components/ticket/steps/CheckoutStep.tsx
+++ src/components/ticket/steps/CheckoutStep.tsx
@@ -96,7 +96,7 @@ export const CheckoutStep = ({ eventsData, onBack }: TicketStepsProps) => {
     setError("");
     setIsLoading(true);
     const stripe = await loadStripe(
-      "pk_test_51Pzc7eHlU79XeHANna3JNwOxLaGCACTmjSvLFutAHYXraytNLgGQtvfI8spk9TGtVxx2oLfyr8M0Y8SbtXFmEMZ700l5gYDEHu"
+      "pk_live_51Pzc7eHlU79XeHANPTYyBdi6WxpUsL6mFHnoKhbvOfcXvabVv3724tALcdPn8YaXVr024oKVv8BV69LRIUSh8TiC00lN3vtJkw"
     );
 
     const body = { basketId };
