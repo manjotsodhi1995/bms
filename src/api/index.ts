8d5a28bdb5703673111af3db8d5e3dad2055be8c
Adway <66119557+adway7103@users.noreply.github.com>
2024-09-19 21:53:18 +0530

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 export const SERVER_BASE_URL = `${
   import.meta.env.VITE_BACKEND_SERVER_URL ||
-  "https://kafsbackend-106f.onrender.com"
+"https://api.kafsco.com"
 }/api/v1` as const;
 
 export const API = {
